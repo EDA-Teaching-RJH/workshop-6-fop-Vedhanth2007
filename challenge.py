09dba3cf0324f2896c94b800696e68c1ef959de8
Vedhanth2007 <vs396@kent.ac.uk>
2026-02-11 12:54:59 +0000

    used while true

--- challenge.py
+++ challenge.py
@@ -1,1 +1,23 @@
-
+rover_state = {
+    "x": 0,
+    "y": 0,
+    "samples": 0
+}
+cmd = input("Is there more in the command batch: ").strip().title()
+if cmd == "Yes":
+    while True:
+        part0 = input("What is the command: ").strip().title()
+        part1 = input("How much space to move")
+        if part0 == "Move":
+            try:
+                space = int(part1)
+            except ValueError:
+                print("bad distance")
+            else:
+                rover_state["y"] = space
+        elif part0 =="Dig":
+            rover_state["samples"] = 1
+        else:
+            print("Unknown command")
+else:
+    print(rover_state)
