19da3ce15adc532407b1e68ff478c9bb7b314741
Vedhanth2007 <vs396@kent.ac.uk>
2026-02-11 11:17:42 +0000

    used a while in part 1

--- part_1.py
+++ part_1.py
@@ -1,1 +1,10 @@
+new_findings = []
+c = 0
+while c < 3:
+    rock = input("Enter rock name: ").strip().title()
+    new_findings.append(rock)
+    c = c + 1
+
+for i in range(len(new_findings)):
+    print(new_findings[i])
 
