0212de934f0d357dc5c652df770e373e14596fec
Vedhanth2007 <vs396@kent.ac.uk>
2026-02-11 12:11:30 +0000

    used try

--- part_4.py
+++ part_4.py
@@ -1,1 +1,19 @@
+travel_log = []
 
+
+while True:
+    try:
+        #num = int(sens)
+        sens = int(input("Enter sensor value: "))
+        if sens < 45: 
+            print(sens)
+            print("path stable")
+            travel_log.append(sens)
+            print(travel_log)
+
+        else:
+            print("CRTICAL TILT HALTING")
+            break
+    except ValueError:
+        print("Sensor glitch")
+print("Mission terminated")
