768e74596ea550eada7d012ec41405c8d94af4d5
Vedhanth2007 <vs396@kent.ac.uk>
2026-02-11 11:27:19 +0000

    used a dictionary

--- part_2.py
+++ part_2.py
@@ -1,1 +1,9 @@
+rover_status = {
+    "Battery": 100,
+    "Heater": "Off"}
+print("Battery charge:", rover_status["Battery"])
+rover_status["Heater"] = "On"
+rover_status["Battery"] = 85
+rover_status["Speed"] = 5
+print(rover_status)
 
